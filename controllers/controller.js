7283f70227c91ef8dfd3ff5b8e090086798ecca2
jyeshthav <jyeshtha.v@somaiya.edu>
2019-01-05 15:29:00 +0530

    fully functional, but shitty navigation and UI

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,7 +9,7 @@ var pigSchema = new mongoose.Schema([{
     item: String
 }])
 var Piggy = mongoose.model('Piggy', pigSchema);
-// var itemOne = Piggy(data).save(function(err){
+// var itemOne = Piggy({item: 'angular'}).save(function(err){
 //     if (err) throw err;
 //     console.log('item saved!');
 // });
@@ -26,7 +26,7 @@ function calculate(a, b, c){
         // console.log("new expense: "+b);
         if (b > a){
             console.log("insufficient funds");
-            return 'gareeb';
+            return total.saved(a,b);
         }
         else{
             // console.log("total saving: "+total.saved(a,b));
@@ -50,20 +50,16 @@ module.exports = function(app){
             res.render('index', {qs: data});
         });
     });
-    app.post('/', urlencodedParser, function(req, res){
+    app.post('/total', urlencodedParser, function(req, res){
         // console.log(req.body);
         Piggy.find({}, function(err, data){
             if (err) throw err;
-            console.log(data[0].item);
-            console.log(data[1].item);
-            console.log(data[2].item);
-            console.log(data[3].item);
 
             // fetch existing from database and update locally
             var travel_e = parseInt(data[0].item) + parseInt(req.body.expense1);
             var food_e = parseInt(data[1].item) + parseInt(req.body.expense2);
             var other_e = parseInt(data[2].item) + parseInt(req.body.expense3);
-            var i = parseInt(data[3].item) + parseInt(req.body.income);
+            var i = parseInt(data[4].item) + parseInt(req.body.income);
 
             // for calculating total expense
             var total_e1 = calculate(travel_e, food_e, 1);
@@ -73,12 +69,37 @@ module.exports = function(app){
             var total_save = calculate(i, total_e2, 2);
 
             // display
-            var total = [travel_e, food_e, other_e, total_e2, total_save];
+            var total = [travel_e, food_e, other_e, total_e2, i, total_save];
             res.render('total', {qst: total});
         });
         
     });
-    app.get('/total', function(req, res){
-        res.render('index');
+    app.post('/dummy', urlencodedParser, function(req, res){
+        // update changes to database
+        Piggy.findByIdAndUpdate({_id: '5c2fd55182678032b0951c48'}, {$set: {item: req.body.update1}}, function (err, data) {
+            if (err) throw (err);
+            console.log("updated");
+        });
+        Piggy.findByIdAndUpdate({_id: '5c2fd5a6fd3ade1db4f2a45e'}, {$set: {item: req.body.update2}}, function (err, data) {
+            if (err) throw (err);
+            console.log("updated");
+        });
+        Piggy.findByIdAndUpdate({_id: '5c2fd5c2ace6a74188d3d754'}, {$set: {item: req.body.update3}}, function (err, data) {
+            if (err) throw (err);
+            console.log("updated");
+        });
+        Piggy.findByIdAndUpdate({_id: '5c2fd5e44fe809431cc264c9'}, {$set: {item: req.body.update4}}, function (err, data) {
+            if (err) throw (err);
+            console.log("updated");
+        });
+        Piggy.findByIdAndUpdate({_id: '5c30748f02a5ec061ce8bd76'}, {$set: {item: req.body.update5}}, function (err, data) {
+            if (err) throw (err);
+            console.log("updated");
+        });
+        Piggy.findByIdAndUpdate({_id: '5c3074c255886942c05915bd'}, {$set: {item: req.body.update6}}, function (err, data) {
+            if (err) throw (err);
+            console.log("updated");
+        });
+        res.render('dummy');
     });
 };
